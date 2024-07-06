8ef579d8b4904bcffd9dc7fac7e0957fc8dbe0e9
iruma-tea <shusuke.think.pad@gmail.com>
2024-07-06 16:06:07 +0900

    Chapter2の学習完了

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Message;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
@@ -15,5 +16,16 @@ class MessageController extends Controller
 
         // messagesというキーで、ビューに渡す
         return view('message/index', ["messages" => $messages]);
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        // リクエストからボディを取得し、保存
+        $message = new Message();
+        $message->body = $request->body;
+        $message->save();
+
+        // 処理後、リダイレクト
+        return redirect('/messages');
     }
 }
