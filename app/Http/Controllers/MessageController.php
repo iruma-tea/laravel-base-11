f5f05a4e671d65e9add418308aba6aac6fb19790
iruma-tea <shusuke.think.pad@gmail.com>
2024-07-06 15:55:25 +0900

    messageの一覧表示を作成

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Message;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class MessageController extends Controller
 {
-    //
+    public function index(): View
+    {
+        // メッセージテーブルのレコードを全件取得
+        $messages = Message::all();
+
+        // messagesというキーで、ビューに渡す
+        return view('message/index', ["messages" => $messages]);
+    }
 }
