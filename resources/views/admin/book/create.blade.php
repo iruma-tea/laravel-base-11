b312aa05cf0a59b3e814c5bf6175441225f5a6d7
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-07-13 10:38:40 +0900

    CHAPTER05までの学習完了

--- resources/views/admin/book/create.blade.php
+++ resources/views/admin/book/create.blade.php
@@ -1,51 +1,37 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <title>書籍登録</title>
-</head>
-<body>
-    <main>
-        <h1>書籍登録</h1>
-        {{-- <form action="/books" method="POST"> --}}
-        {{-- @if ($errors->any())
-            <div style="color:red">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif --}}
-        <x-alert>
+<x-layouts.book-manager>
+    <x-slot:title>
+        書籍登録
+    </x-slot:title>
+    <h1>書籍登録</h1>
+    @if ($errors->any())
+        <x-alert class="danger">
             <x-error-messages :errors="$errors" />
         </x-alert>
-        <form action="{{ route('book.store') }}" method="POST">
-                @csrf
-            <div>
-                <label>カテゴリ</label>
-                <select name="category_id">
-                    @foreach ($categories as $category)
-                        {{-- <option value="{{ $category->id }}">
-                            {{ $category->title }}
-                        </option> --}}
-                        <option value="{{ $category->id }}"
-                            @selected($category->id == old('category_id'))>
-                            {{ $category->title }}
-                        </option>
-                    @endforeach
-                </select>
-            </div>
-            <div>
-                <label>タイトル</label>
-                <input type="text" name="title" value="{{ old('title') }}">
-            </div>
-            <div>
-                <label>価格</label>
-                <input type="text" name="price" value="{{ old('price') }}">
-            </div>
-            <input type="submit" value="送信">
-        </form>
-    </main>
-</body>
-</html>
+    @endif
+    <form action="{{ route('book.store') }}" method="POST">
+            @csrf
+        <div>
+            <label>カテゴリ</label>
+            <select name="category_id">
+                @foreach ($categories as $category)
+                    {{-- <option value="{{ $category->id }}">
+                        {{ $category->title }}
+                    </option> --}}
+                    <option value="{{ $category->id }}"
+                        @selected($category->id == old('category_id'))>
+                        {{ $category->title }}
+                    </option>
+                @endforeach
+            </select>
+        </div>
+        <div>
+            <label>タイトル</label>
+            <input type="text" name="title" value="{{ old('title') }}">
+        </div>
+        <div>
+            <label>価格</label>
+            <input type="text" name="price" value="{{ old('price') }}">
+        </div>
+        <input type="submit" value="送信">
+    </form>
+</x-layouts.book-manager>
