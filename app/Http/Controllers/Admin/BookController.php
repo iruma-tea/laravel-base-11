fe627ef3d328e8eb210f09e8afe1036ef04d487f
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-07-13 16:05:40 +0900

    更新機能を追加

--- app/Http/Controllers/Admin/BookController.php
+++ app/Http/Controllers/Admin/BookController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\BookPostRequest;
+use App\Http\Requests\BookPutRequest;
 use App\Models\Author;
 use App\Models\Book;
 use App\Models\Category;
@@ -63,5 +64,37 @@ class BookController extends Controller
 
         // 保存した書籍情報をレスポンスとして返す。
         return redirect(route('book.index'))->with('message', $book->title . 'を追加しました。');
+    }
+
+    public function edit(Book $book): View
+    {
+        // カテゴリ一覧を表示するために全件取得
+        $categories = Category::all();
+
+        // 著者一覧を表示するために全件取得
+        $authors = Author::all();
+
+        // 書籍に紐づく著者IDの一覧を取得
+        $authorIds = $book->authors()->pluck('id')->all();
+
+        return view('admin/book/edit', compact('book', 'categories', 'authors', 'authorIds'));
+    }
+
+    public function update(BookPutRequest $request, Book $book): RedirectResponse
+    {
+        // リクエストオブジェクトからパラメータを取得
+        $book->category_id = $request->category_id;
+        $book->title = $request->title;
+        $book->price = $request->price;
+
+        DB::transaction(function () use ($book, $request) {
+            // 更新
+            $book->update();
+
+            // 書籍と著者の関連付けを更新する
+            $book->authors()->sync($request->author_ids);
+        });
+
+        return redirect(route('book.index'))->with('message', $book->title . "を更新しました。");
     }
 }
